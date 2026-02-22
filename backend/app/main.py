7ab52a83b43180b8108ab31b5a07312c58507c13
Lakshya Gupta <lakshyagupta301206@gmail.com>
2026-02-21 23:03:51 -0600

    Made a basic working openCV model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,20 @@
-# Starts FastAPI app, registers routes, middleware, and initializes services.
+
+from fastapi import FastAPI
+from app.api.v1 import plan_routes, prediction_routes
+from app.api.v1.dev_routes import router as dev_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # dev only
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Register main routers
+app.include_router(plan_routes.router)
+app.include_router(prediction_routes.router)
+app.include_router(dev_router)
